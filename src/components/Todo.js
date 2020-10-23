bef37f3cfcf593921ea9084cd8c85e7f49433971
jonatasmota <jonatascmota@gmail.com>
2020-10-23 17:49:22 -0300

    Easy Todo Initial

---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Todo = ({text, setTodos, todos}) => {
+    //Events
+    const deleteHandler = () => {
+        setTodos (todos.filter(el => ))
+    }
+    return (
+        <div className="todo">
+            <li className="todo-item">{text}</li>
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+export default Todo;
